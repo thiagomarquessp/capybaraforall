4362413f9cd0592cacde8b74cabb3b563660036c
Thiago Biro Biro <thiagomarquessp@gmail.com>
2017-01-23 09:26:31 -0200

    add chromedriver

--- cucumber/features/support/env.rb
+++ cucumber/features/support/env.rb
@@ -1,5 +1,9 @@
 require "capybara/cucumber"
 require "selenium-webdriver"
 
+Capybara.register_driver :selenium do |app|
+  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+end
+
 Capybara.default_driver = :selenium
 Capybara.default_max_wait_time = 60
